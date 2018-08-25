6ac3ac5860e72989e4fd1f53acf730f56dbc9258
terry <terry@meishakeji.com>
2018-08-25 11:57:26 +0800

    使用Logrus记录log

---
+++ libs/perf/perf.go
@@ -0,0 +1,26 @@
+package perf
+
+import (
+	"net/http"
+	"net/http/pprof"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// StartPprof start http pprof.
+func Init(pprofBind []string) {
+	pprofServeMux := http.NewServeMux()
+	pprofServeMux.HandleFunc("/debug/pprof/", pprof.Index)
+	pprofServeMux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	pprofServeMux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	pprofServeMux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	for _, addr := range pprofBind {
+		go func() {
+			if err := http.ListenAndServe(addr, pprofServeMux); err != nil {
+				log.Panic("I'm bailing.") //log之后会panic()
+				log.Error("http.ListenAndServe(\"%s\", pprofServeMux) error(%v)", addr, err)
+				panic(err)
+			}
+		}()
+	}
+}
