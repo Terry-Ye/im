fb5f25ca18d447eb4e6290a819ea56abfc08d948
terry <terry@meishakeji.com>
2019-01-03 23:28:07 +0800

    使用https与wss

--- demo/main.go
+++ demo/main.go
@@ -7,5 +7,9 @@ import (
 
 func main() {
 	// Simple static webserver:
-	log.Fatal(http.ListenAndServe(":1999", http.FileServer(http.Dir("./"))))
+	// log.Fatal(http.ListenAndServe(":1999", http.FileServer(http.Dir("./"))))
+	log.Fatal(http.ListenAndServeTLS(":1999","/etc/letsencrypt/live/texixi.com-0002/fullchain.pem", "/etc/letsencrypt/live/texixi.com-0002/privkey.pem", http.FileServer(http.Dir("./"))))
+
+
+
 }
